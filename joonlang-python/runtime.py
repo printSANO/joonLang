07d386a64ba72066510d7bd27aa07ea1420d8d71
Ryan <leehahnwoong@gmail.com>
2023-09-10 01:26:20 -0700

    feat: basic structure and test code for .joon
    functions for parsing joon code needs to be finished.
    
    Issue: #1

--- joonlang-python/runtime.py
+++ joonlang-python/runtime.py
@@ -5,6 +5,7 @@ class Joon:
         self.data = {}
         self.start_marker = "하시면 어떻게든 됩니다."
         self.end_marker = "부트캠프 멘토 그만해야지."
+        self.operators = ["네?", "에?", "워터밤"]
     
     def read_joon_file(self, filename) -> None:
         try:
@@ -14,8 +15,8 @@ class Joon:
                 if val:
                     for line in lines:
                         l = line.strip()
-                        if l != self.start_marker and l != self.end_marker and l != '':
-                            print(l)
+                        if l != self.start_marker and l != self.end_marker and l != '' and not l.startswith('#'):
+                            self.check_type(l)
                 else:
                     if message == self.start_marker:
                         print("에러: 어떻게 하셔도 안되네요?")
@@ -27,7 +28,7 @@ class Joon:
     
     def validate_joon_code(self, lines) -> bool and str:
         if len(lines) < 2:
-            return False
+            return False, "여러분 그럴 시간에 코드 한 줄 더 쓰세요."
         
         start_marker = "하시면 어떻게든 됩니다."
         end_marker = "부트캠프 멘토 그만해야지."
@@ -38,5 +39,39 @@ class Joon:
             return False, end_marker
         
         return True, None
+    
+    # calculate values from 네?, 에?, and 워터밤
+    def calculate_val(self, line) -> int:
+        pass
 
-        
+    def check_type(self, line) -> str:
+        if "님이말씀해보세요" in line:
+            pass
+        elif "님?" in line:
+            pass
+        elif "자러가시는거에요" in line:
+            pass
+        elif "님맥사세요" in line:
+            pass
+        elif "그냥여쭤^^보는거에요" in line:
+            pass
+    
+    # 님이말씀해보세요
+    def parse_var(self, line):
+        pass
+
+    # 님?
+    def parse_assign(self, line):
+        pass
+
+    # 자러가시는거에요
+    def parse_loop(self, line):
+        pass
+
+    # 님맥사세요
+    def parse_output(self, line):
+        pass
+
+    # 그냥여쭤^^보는거에요
+    def parse_condition(self, line):
+        pass
